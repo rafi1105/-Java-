5d3fbab2e9265e33fc97952a4496d81a0a16d527
rafi1105 <111092930+rafi1105@users.noreply.github.com>
2023-02-10 00:51:54 +0600

    rafi

--- Learn Java/HelloWorld.java
+++ Learn Java/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
+
+
   public static void main(String[] args) {
-      System.out.println("hello java");
+    System.out.println("HELLOWORLD");
+    System.out.println("HELLOWORLD");
   }
+
 }
---
+++ Learn Java/Variable/index.java
@@ -0,0 +1,26 @@
+// int  integer nubmer  { 4,6}
+// float fracment number { 4.56}
+// char character { 'a','5'}
+// boolean is { true or false }
+public class index {
+    public static  void main ( String [] args){
+        int x=5, y=6;
+        char c = 'a';
+        System.out.println("it's is  index");
+        // type 1 print integer
+        System.out.print("I am a integer number  : ");
+        System.out.println(x);
+        System.out.println("OR");
+        // type 2 print integer
+        System.out.println("Integer nubmer : " + x);
+        System.out.println(c);
+        // string now !!
+        String firstName = "rafi";
+        String lastName = "kabir";
+        String Name=firstName + " " + lastName;
+        System.out.println(Name);
+        // sum in print  line
+        System.out.println("Sum is : " + (x+y));
+    }
+
+}
