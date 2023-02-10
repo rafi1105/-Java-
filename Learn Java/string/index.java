1373071a12178a9e15cf52f2127e8169a8bf3a28
Rafi_kabir <111092930+rafi1105@users.noreply.github.com>
2023-02-10 14:33:50 +0600

    rk1

---
+++ Learn Java/string/index.java
@@ -0,0 +1,13 @@
+package string;
+
+public class index { 
+    public static void main ( String[] args ){
+        String text= "I am a new programmmer";
+        // Counting lenght of a string
+        System.out.println("The length of text is " + text.length());
+        // type to upper
+        System.out.println(text.toUpperCase());
+        // type to lower
+        System.out.println(text.toLowerCase());
+    }
+}
